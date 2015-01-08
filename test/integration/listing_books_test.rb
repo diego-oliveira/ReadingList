ac2ab772c7cecc96753d1f6a09c664175e275a6b
Diego Oliveira <diego.oliveira@rga.com>
2015-01-08 18:28:09 -0200

    refactoring json parse to a method helper

--- test/integration/listing_books_test.rb
+++ test/integration/listing_books_test.rb
@@ -10,7 +10,7 @@ class ListingBooksTest < ActionDispatch::IntegrationTest
     get '/books'
     assert_equal 200, response.status
     assert_equal Mime::JSON, response.content_type
-    assert_equal Book.count, JSON.parse(response.body).size
+    assert_equal Book.count, json(response.body).size
   end
   
   test 'should list tip rated books' do
@@ -18,6 +18,6 @@ class ListingBooksTest < ActionDispatch::IntegrationTest
 
     assert_equal 200, response.status
     assert_equal Mime::JSON, response.content_type
-    assert_equal 1, JSON.parse(response.body).size
+    assert_equal 1, json(response.body).size
   end
 end
