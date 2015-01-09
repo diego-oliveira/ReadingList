db5502da71eb06dc4670a59b8a4d89e3c494734b
Diego Oliveira <oliveira.bsi@gmail.com>
2015-01-09 14:33:27 -0200

    create test for invalid data

--- test/integration/creating_books_test.rb
+++ test/integration/creating_books_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class CreatingBooksTest < ActionDispatch::IntegrationTest
-  test 'should new book' do
+  test 'should create new book with valid data' do
     post '/books', {book: {
       title: 'Pragmatic Programmer',
       rating: 5
@@ -13,5 +13,15 @@ class CreatingBooksTest < ActionDispatch::IntegrationTest
     assert_equal book_url(book[:id]), response.location
     assert_equal 'Pragmatic Programmer', book[:title]
     assert_equal 5, book[:rating].to_i
+  end
+
+  test 'should doest not create new book with invalid data' do
+    post '/books', {book: {
+      title: nil,
+      rating: 5
+    }}.to_json, {'Accept' => 'application/json', 'Content-Type' => 'application/json'}
+
+    assert_equal 422, response.status
+    assert_equal Mime::JSON, response.content_type
   end
 end
