ef56e6f7d1207e1c4026c66896599e3616388a0b
Diego Oliveira <dega@diegos-mbp.home>
2015-01-09 00:38:59 -0200

    add create book endpoint

---
+++ test/integration/creating_books_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class CreatingBooksTest < ActionDispatch::IntegrationTest
+  test 'should new book' do
+    post '/books', {book: {
+      title: 'Pragmatic Programmer',
+      rating: 5
+    }}.to_json, {'Accept' => 'application/json', 'Content-Type' => 'application/json'}
+
+    assert_equal 201, response.status
+    assert_equal Mime::JSON, response.content_type
+    book = json(response.body)
+    assert_equal book_url(book[:id]), response.location
+    assert_equal 'Pragmatic Programmer', book[:title]
+    assert_equal 5, book[:rating].to_i
+  end
+end
